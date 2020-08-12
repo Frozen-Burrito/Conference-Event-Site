f20465884ebed6cfe5f5165514a05e08e10f685f
FrozenBurrito <f3rm3ndouza@outlook.com>
2020-08-11 21:50:15 -0500

    MongoDB connection
    Setup the connection to MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
+const mongoose = require('mongoose');
+
 const indexRouter = require('./Routes/index');
 
 const app = express();
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({ path: './.env' });
+}
 
 // View Engine 
 app.set('view engine', 'ejs');
@@ -14,6 +20,16 @@ app.use(expressLayouts);
 
 // Static files
 app.use(express.static('Public'));
+
+// Connect DB
+mongoose.connect(process.env.MONGODB_URL, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to MongoDB'));
 
 // Routes
 app.use('/', indexRouter);
